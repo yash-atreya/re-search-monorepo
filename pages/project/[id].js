e621f05b4c61b4a678043575490890e4f8b5fe11
Yash Atreya <yashatreya.ya@icloud.com>
2021-05-26 14:21:20 +0530

    removed api-keys regenerate button

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -237,14 +237,14 @@ function Project({ projectID, projectData, initial_indices, apiKeys, error }) {
                       )}
 
                       <div className="mt-10">
-                        <button
+                        {/* <button
                           type="button"
                           className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
                           style={{ marginRight: 12 }}
                           onClick={closeModal}
                         >
                           Regenerate
-                        </button>
+                        </button> */}
                         <button
                           type="button"
                           className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
